af677538e0ae99754f84ae38bdffac3dad022cbb
nodeU <nodeU@server01.fosocail.com>
2020-07-01 20:31:48 +0000

    update 103

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Install dependencies for nodejs if any.
-cd /var/www/vhosts/test/citest && npm install
+cd /var/www/vhosts/test/citest
+git pull origin master
+npm install
 echo -e "\n\nInstalling Nodejs Dependencies.\n"
